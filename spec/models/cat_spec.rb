7f7e078cc24624f36f1f7570b0e87dba651eb4e9
kevans107 <kellyevans107.gmail.com>
2021-11-02 13:09:07 -0400

    updated api-validations

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -13,4 +13,8 @@ RSpec.describe Cat, type: :model do
     cat = Cat.create(name: 'Kitty', age: 3)
     expect(cat.errors[:enjoys]).to_not be_empty
   end
+  it 'should have an enjoys entry of at least 10 characters' do
+    cat = Cat.create(name: 'Kitty', age: 3, enjoys: 'food')
+    expect(cat.errors[:enjoys].first).to eq 'is too short (minimum is 10 characters)'
+  end
 end
